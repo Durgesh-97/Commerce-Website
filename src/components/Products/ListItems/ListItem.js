f9adff0846d9a43e6196c62be999392b93446cd3
Durgesh Chaudhari <durgesh.project96@gmail.com>
2024-05-05 20:55:07 +0530

    Added Counter (Conditional Rendering)

--- src/components/Products/ListItems/ListItem.js
+++ src/components/Products/ListItems/ListItem.js
@@ -1,6 +1,23 @@
+import { useState } from "react"
 import AddToCartIcon from "../../../assets/icons/add_cart.svg"
 
 const ListItem = ({ data }) => {
+    const [counter, setCounter] = useState(0)
+
+    const increaseCounterByOne = () => {
+        setCounter(counter + 1)
+    }
+
+    const decreaseCounterByOne = () => {
+        if(counter === 0){
+            return;
+        }
+            
+            setCounter(counter - 1)
+    } 
+    const handleClick = () => {
+        //Handle button click
+    }
     return (
         <div className={"item-card"}>
             <img className={"img-fluid"} src={`/assets/${data.thumbnail}`} alts={`data.title`} />
@@ -14,10 +31,16 @@ const ListItem = ({ data }) => {
                 <div className={"title"}>
                     <h3>{data.titles}</h3>
                 </div>
-                <button className={"cart-add"}>
+                {/* <button className={"cart-add"}>
                     <span>Add to cart</span>
                     <img src={AddToCartIcon} alt="Cart Icon" />
-                </button>
+                </button> */}
+                <div className="cart-addon">
+                    <button onClick={decreaseCounterByOne}><span>-</span></button>
+                    <span>{counter}</span>
+                    <button onClick={increaseCounterByOne}><span>+</span></button>
+
+                </div >
             </div>
         </div>
     )

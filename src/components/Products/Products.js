793df3c989547458b9a99984d4b6a988506475c2
Durgesh Chaudhari <durgesh.project96@gmail.com>
2024-05-05 19:21:28 +0530

    Rendering of list

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,29 +1,53 @@
 // import ListItem from "./components/Products/ListItems/ListItem";
+import { useState } from "react";
 import ListItem from "./ListItems/ListItem";
 
-const items = [
-    {
-        id: 0,
-        discountedPrice: 340,
-        price: 450,
-        titles: "Title of the Item",
-        thumbnail: "placeholder.png"
-    },
-    {
-        id: 1,
-        discountedPrice: 160,
-        price: 350,
-        titles: "Title of the Item 2",
-        thumbnail: "placeholder.png"
-    }
-]
 
 const Products = () => {
+    const [items, setItems] = useState(
+        [
+            {
+                id: 0,
+                discountedPrice: 340,
+                price: 450,
+                titles: "Title of the Item",
+                thumbnail: "placeholder.png"
+            },
+            {
+                id: 1, 
+                discountedPrice: 160,
+                price: 350,
+                titles: "Title of the Item 2",
+                thumbnail: "placeholder.png"
+            },
+            {
+                id: 2, 
+                discountedPrice: 160,
+                price: 350,
+                titles: "Title of the Item 3",
+                thumbnail: "placeholder.png"
+            },
+            {
+                id: 3, 
+                discountedPrice: 160,
+                price: 350,
+                titles: "Title of the Item 4",
+                thumbnail: "placeholder.png"
+            }
+        ]
+    )
     return (
         <div className={"product-list"}>
             <div className={"product-list--wrapper"}>
-                <ListItem data={items[0]}></ListItem>
-                <ListItem data={items[1]}></ListItem>
+                {/* <ListItem data={items[0]}></ListItem>
+                <ListItem data={items[1]}></ListItem> */}
+              {
+                items.map(item => {
+                    console.log(item);
+                    return (<ListItem data={item}/>)
+                })
+              }
+
             </div>
         </div>
     )

03c47e5497f9193129924ee7a625df12609aec09
Durgesh Chaudhari <durgesh.project96@gmail.com>
2024-05-10 15:45:02 +0530

    Handling data from Database

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,51 +1,30 @@
 // import ListItem from "./components/Products/ListItems/ListItem";
 import { useEffect, useState } from "react";
 import ListItem from "./ListItems/ListItem";
-// import { response } from "express";
 import axios from "axios";
 
 const Products = () => {
-    const [items, setItems] = useState(
-        [
-            {
-                id: 0,
-                discountedPrice: 340,
-                price: 450,
-                titles: "Title of the Item",
-                thumbnail: "placeholder.png"
-            },
-            {
-                id: 1, 
-                discountedPrice: 160,
-                price: 350,
-                titles: "Title of the Item 2",
-                thumbnail: "placeholder.png"
-            },
-            {
-                id: 2, 
-                discountedPrice: 160,
-                price: 350,
-                titles: "Title of the Item 3",
-                thumbnail: "placeholder.png"
-            },
-            {
-                id: 3, 
-                discountedPrice: 160,
-                price: 350,
-                titles: "Title of the Item 4",
-                thumbnail: "placeholder.png"
-            }
-        ])
+    const [items, setItems] = useState([])
 
         useEffect(() => {
-        //    const result = fetch('https://e-commerce-c2a22-default-rtdb.firebaseio.com/items.json')
+        //    fetch('https://e-commerce-c2a22-default-rtdb.firebaseio.com/items.json')
         //    .then(response => response.json())
         //    .then(data => {
         //     console.log(error);
-        //    })        
+        //    })
+
         axios.get('https://e-commerce-c2a22-default-rtdb.firebaseio.com/items.json')
         .then(response => {
             console.log(response);
+            const data = response.data
+            const transformData = data.map((item, index) => {
+                return{
+                    ...item,
+                    id: index
+                }
+            })
+            setItems(transformData)
+            
         })
         .catch(error => {
             console.log(error);

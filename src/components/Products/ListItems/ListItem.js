b4555547453f591893fa973005e781a341dbd821
Durgesh Chaudhari <durgesh.project96@gmail.com>
2024-05-14 21:39:57 +0530

    Updated CSS & added Counter in Modal

--- src/components/Products/ListItems/ListItem.js
+++ src/components/Products/ListItems/ListItem.js
@@ -1,77 +1,102 @@
-
-import { Fragment, useState } from "react"
-import AddToCartIcon from "../../../assets/icons/add_cart.svg"
-import Modal from "../../UI/Modal"
+import { Fragment, useState } from "react";
+import AddToCartIcon from "../../../assets/icons/add_cart.svg";
+import Modal from "../../UI/Modal";
 
 const ListItem = ({ data, updateItemTitle }) => {
-    const [counter, setCounter] = useState(0)
-    const [showModal, setShowModal] = useState(false)
+  const [counter, setCounter] = useState(0);
+  const [showModal, setShowModal] = useState(false);
 
-    const increaseCounterByOne = () => {
-        setCounter(counter+1)
-    }
+  const increaseCounterByOne = () => {
+    setCounter(counter + 1);
+  };
 
-    const decreaseCounterByOne = () => {
-        if(counter === 0) {
-            return;
-        }
-        setCounter(counter-1)
+  const decreaseCounterByOne = () => {
+    if (counter === 0) {
+      return;
     }
-    const handleModal = () =>{
-        setShowModal(previousState => !previousState)
-    }
+    setCounter(counter - 1);
+  };
+  const handleModal = () => {
+    setShowModal((previousState) => !previousState);
+  };
 
-    return (
-        <Fragment>
-        <div onClick={handleModal} className={"item-card"}>
-            <img className={"img-fluid"} src={`/assets/${data.thumbnail}`} alt={data.title}/>
-            <div className={"item-card__information"}>
-                <div className={"pricing"}>
-                    <span>₹{data.discountedPrice}</span>
-                    <small>
-                        <strike>₹{data.price}</strike>
-                    </small>
-                </div>
-                <div className={"title"}>
-                    <h3>{data.title}</h3>
-                </div>
-            </div>
-            <button onClick={() => updateItemTitle(data.id)}>Update the title</button>
-            {
-                counter < 1 ?
-                <button className={"cart-add"} onClick={increaseCounterByOne}>
-                    <span>Add to Cart</span>
-                    <img src={AddToCartIcon} alt="Cart Icon"/>
-                </button>
-                :
-                <div className="cart-addon">
-                    <button onClick={decreaseCounterByOne}><span>-</span></button>
-                    <span>{counter}</span>
-                    <button onClick={increaseCounterByOne}><span>+</span></button>
-                </div>
-            }
-        </div>
-        { showModal && 
-        <Modal onClose={handleModal}> 
-        <div className="item-card__modal">
-          <div className="img-wrap">
-             <img className={"img-fluid"} src={`/assets/${data.thumbnail}`} alt={data.title}/>
-           </div>
-             <div className="meta">
-              <h3>{data.title}</h3>
-                 <div className={"pricing"}>
-                    <span>₹{data.discountedPrice}</span>
-                    <small>
-                        <strike>₹{data.price}</strike>
-                    </small>
-            </div> 
-                <p>{data.description}</p>         
+  return (
+    <Fragment>
+      <div onClick={handleModal} className={"item-card"}>
+        <img
+          className={"img-fluid"}
+          src={`/assets/${data.thumbnail}`}
+          alt={data.title}
+        />
+        <div className={"item-card__information"}>
+          <div className={"pricing"}>
+            <span>₹{data.discountedPrice}</span>
+            <small>
+              <strike>₹{data.price}</strike>
+            </small>
           </div>
-      </div>        
-        </Modal>
+          <div className={"title"}>
+            <h3>{data.title}</h3>
+          </div>
+        </div>        
+        {counter < 1 ? 
+          <button className={"cart-add"} onClick={increaseCounterByOne}>
+            <span>Add to Cart</span>
+            <img src={AddToCartIcon} alt="Cart Icon" />
+          </button>
+         : 
+          <div className="cart-addon">
+            <button onClick={decreaseCounterByOne}>
+              <span>-</span>
+            </button>
+            <span>{counter}</span>
+            <button onClick={increaseCounterByOne}>
+              <span>+</span>
+            </button>
+          </div>
         }
-        </Fragment>
-    )
-}
+      </div>
+      {showModal && (
+        <Modal onClose={handleModal}>
+          <div className="item-card__modal">
+            <div className="img-wrap">
+              <img
+                className={"img-fluid"}
+                src={`/assets/${data.thumbnail}`}
+                alt={data.title}
+              />
+            </div>
+            <div className="meta">
+              <h3>{data.title}</h3>
+              <div className={"pricing"}>
+                <span>₹{data.discountedPrice}</span>
+                <small>
+                  <strike>₹{data.price}</strike>
+                </small>
+              </div>
+              <p>{data.description}</p>
+              {counter < 1 ? 
+          <button className={"cart-add"} onClick={increaseCounterByOne}>
+            <span>Add to Cart</span>
+            <img src={AddToCartIcon} alt="Cart Icon" />
+          </button>
+         : 
+          <div className="cart-addon">
+            <button onClick={decreaseCounterByOne}>
+              <span>-</span>
+            </button>
+            <span>{counter}</span>
+            <button onClick={increaseCounterByOne}>
+              <span>+</span>
+            </button>
+          </div>
+        }
+            </div>
+          </div>
+        </Modal>
+      )}
+    </Fragment>
+  );
+};
 
-export default ListItem
+export default ListItem;

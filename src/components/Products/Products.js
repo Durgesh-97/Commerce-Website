f597f650f39bae87e76d41ed13e80e5d2fca3ab0
Durgesh Chaudhari <durgesh.project96@gmail.com>
2024-05-15 00:06:50 +0530

    Error Fix

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,16 +1,16 @@
-import { useEffect, useState } from "react";
-import ListItem from "./ListItems/ListItem";
-import axios from "axios";
-import Loader from "../UI/Loader";
+import { useEffect, useState } from "react"
+import ListItem from "./ListItems/ListItem"
+import axios from "axios"
+import Loader from "../UI/Loader"
 
-const Products = ({ onAddItem, OnRemoveItem }) => {
+const Products = ({ onAddItem, onRemoveItem }) => {
     const [items, setItems] = useState([])
     const [loader, setLoader] = useState(true)
     const [presentItems, setPresentItems] = useState([])
 
         useEffect(() => {      
-        async function fetchItems () {
-            try{                
+           async function fetchItems () {
+            try {                
                 const response = await axios.get('https://e-commerce-c2a22-default-rtdb.firebaseio.com/items.json')
                 const data = response.data
                 const transformData = data.map((item, index) => {
@@ -35,21 +35,31 @@ const Products = ({ onAddItem, OnRemoveItem }) => {
     }, [])
 
     const handleAddItem = id => {
-        if(presentItems.indexOf(id)> -1){
+        if(presentItems.indexOf(id) > -1) {
             return;
         }
         setPresentItems([...presentItems, id])
         onAddItem();
     }
+
     const handleRemoveItem = id => {
         let index = presentItems.indexOf(id)
         if(index > -1) {
             let items = [...presentItems]
             items.splice(index, 1)
             setPresentItems([...items]);
-            OnRemoveItem()    
+            onRemoveItem();    
         }        
-    } 
+    }
+    // const handleRemoveItem = id => {
+    //     let index = presentItems.indexOf(id)
+    //     if(index > -1) {
+    //         let items = [...presentItems]
+    //         items.splice(index, 1)
+    //         setPresentItems([...items]);
+    //         onRemoveItem();
+    //     }
+    // } 
 
     return (
         <>
@@ -59,17 +69,16 @@ const Products = ({ onAddItem, OnRemoveItem }) => {
                 <ListItem data={items[1]}></ListItem> */}
               {
                 items.map(item => {                    
-                    return (<ListItem onAdd={handleAddItem} OnRemove={handleRemoveItem} key={item.id} data={item} />) 
+                    return (<ListItem onAdd={handleAddItem} onRemove={handleRemoveItem} key={item.id} data={item}/>) 
                 })
               }
               {/* {[<ListItem data={item[0]}/>,<ListItem data={item[1]}/>,<ListItem data={item[2]}/>]} Render pattern List */}
 
             </div>
         </div>
-        { loader && <Loader />}
+        { loader && <Loader/>}
         </>
     )
 }
 
 export default Products
-

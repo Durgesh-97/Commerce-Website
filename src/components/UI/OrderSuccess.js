d9d926014cf56ec565dce673e0fdfbbb2396ce3c
Durgesh Chaudhari <durgesh.project96@gmail.com>
2024-05-16 21:03:07 +0530

    Added Order Now Functionality & Order Success Modal

---
+++ src/components/UI/OrderSuccess.js
@@ -0,0 +1,22 @@
+import Modal from "./Modal"
+import OrderSuccessImage from "../../assets/icons/order_Success.svg"
+
+const OrderSuccessModal = ({onClose}) => {
+    return(
+        <Modal onClose={onClose}>
+            <div className="order-container">
+                <div className="order-container--success">
+                    <img src={OrderSuccessImage} alt="Success" className="img-fluid"/>
+                    <div className="message">
+                        <h1>Your Order Placed Successfully! </h1>
+                        <span>OrderId: {Math.random().toString(32).slice(2)}</span>
+                    </div>                    
+                </div>
+
+            </div>
+
+        </Modal>
+    )
+}
+
+export default OrderSuccessModal

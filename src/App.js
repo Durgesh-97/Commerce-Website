da2f17105c80799649b9e88ccec2fe98f9d938b5
Durgesh Chaudhari <durgesh.project96@gmail.com>
2024-05-16 15:11:34 +0530

    Added Functionality in Cart Modal

--- src/App.js
+++ src/App.js
@@ -2,19 +2,37 @@ import Products from "./components/Products/Products";
 import Header from "./components/Layout/Header"
 import Subheader from "./components/Layout/subheader";
 import { useState } from "react";
-
+  
 const App = () => {
-  const [cartItems, setCartItems] = useState(0)
+  const [cartItems, setCartItems] = useState([])
 
-  const handleAddItem = () => {
-    setCartItems(cartItems + 1)
+  const handleAddItem = item => {
+    let items = [...cartItems]
+    let index = items.findIndex(i => i.id === item.id)
+    if(index > -1){
+      item[index] = item
+
+    }
+    else{
+      items.push(item)
+    }
+    setCartItems([...items])
+    // setCartItems(cartItems + 1)
   }
-  const handleRemoveItem = () => {
+  const handleRemoveItem = item => {
+    let items = [...cartItems]
+    let index = items.findIndex(i => i.id === item.id)
+    if(items[index].quantity === 0){
+      items.splice(index, 1)
+    }
+    else{
+      items[index] = item
+    }
     setCartItems(cartItems - 1)
   }
   return (
     <div>
-      <Header count={cartItems} />
+      <Header count={cartItems.length} items={cartItems}/>
       <Subheader />
       <Products onAddItem={handleAddItem} onRemoveItem={handleRemoveItem}/>
     </div>
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -16,6 +16,7 @@ const Products = ({ onAddItem, onRemoveItem }) => {
                 const transformData = data.map((item, index) => {
                     return{
                         ...item,
+                        quantity: 0,
                         id: index
                 }
             })
@@ -35,20 +36,32 @@ const Products = ({ onAddItem, onRemoveItem }) => {
     }, [])
 
     const handleAddItem = id => {
-        if(presentItems.indexOf(id) > -1) {
-            return;
-        }
-        setPresentItems([...presentItems, id])
-        onAddItem();
+        // if(presentItems.indexOf(id) > -1) {
+        //     return;
+        // }
+        // setPresentItems([...presentItems, id])
+        
+        let data = [...items]
+        let index = data.findIndex(i => i.id === id)
+        data[index].quantity += 1
+        setItems([...data])
+        onAddItem(data[index]);
     }
 
     const handleRemoveItem = id => {
-        let index = presentItems.indexOf(id)
-        if(index > -1) {
-            let items = [...presentItems]
-            items.splice(index, 1)
-            setPresentItems([...items]);
+        // let index = presentItems.indexOf(id)
+        // if(index > -1) {
+        //     let items = [...presentItems]
+        //     items.splice(index, 1)
+        //     setPresentItems([...items]);
             onRemoveItem();    
+        // }
+        let data = [...items]
+        let index = data.findIndex(i => i.id === id)        
+        if (data[index].quantity !== 0){
+            data[index].quantity -= 1
+            setItems([...data])
+            onRemoveItem(data[index]);
         }        
     }
 

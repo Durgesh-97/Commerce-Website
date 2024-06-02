6b12fc6b0198620f48716df98193f8af8a4a6104
Durgesh Chaudhari <durgesh.project96@gmail.com>
2024-06-02 13:00:02 +0530

    Minor Fixes

--- src/components/Layout/subheader.js
+++ src/components/Layout/subheader.js
@@ -6,7 +6,7 @@ const Subheader = () => {
         <ul>
           <li><NavLink exact to="/">Home</NavLink></li>
           <li><NavLink exact to="/category-1">Category 1</NavLink></li>
-          <li><NavLink exact to="/category-2">Category 2</NavLink></li>
+          <li><NavLink exact to="/category-2">Category 2</NavLink></li>          
           {/* <li><a href="">Home</a></li>
                 <li><a href="">Category 1</a></li>
                 <li><a href="">Category 2</a></li>
